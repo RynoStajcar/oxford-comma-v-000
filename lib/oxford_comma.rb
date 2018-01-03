44802726a50cd38581f5d39cc220c381c28b812f
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-03 16:23:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size > 2
-    array.join(", ") + array.join(" and ")
+    array.join(", ") + array[-2] = (" and ")
   end
 end

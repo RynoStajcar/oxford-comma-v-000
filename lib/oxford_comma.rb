07d415e8db27f45e52ceb54d09a114da59bca6be
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-03 15:25:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
-  if array.size == 2
-    array.join(" and ")
-  elsif array.size > 2
-    array.split(",")
-    array[-1].join("and")
+  if array.size < 2
+    array.join
+  elsif array.size == 2
+    array.split(" and ")
   end
 end
